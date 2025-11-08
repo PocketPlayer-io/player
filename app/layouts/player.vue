56ec2572c0152c744c24814b5887f76372187177
jonathan-martz <github@jmartz.de>
2025-11-08 17:20:21 +0100

    work on mvp

---
+++ app/layouts/player.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="min-h-screen bg-red-400 py-3 px-3">
+    <slot></slot>
+  </main>
+</template>
+<script setup lang="ts">
+</script>
--- app/pages/player.vue
+++ app/pages/player.vue
@@ -1,9 +1,12 @@
 <template>
   <iframe id="player" type="text/html" width="100%" class="aspect-16/9"
-          :src="'https://www.youtube.com/embed/'+route.query.id+'?enablejsapi=1&autoplay=1'"
-          frameborder="0"></iframe>
+          :src="'https://www.youtube.com/embed/'+route.query.id+'?enablejsapi=1&controls=0'"
+          frameborder="0"  allow="autoplay; fullscreen; encrypted-media"></iframe>
 </template>
 
 <script setup lang="ts">
+definePageMeta({
+  'layout': 'player'
+})
 const route = useRoute();
 </script>#
