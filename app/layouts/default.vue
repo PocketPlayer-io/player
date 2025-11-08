4d3b541d8d70ceb0d29cb2412752f88eed38a579
jonathan-martz <github@jmartz.de>
2025-11-08 14:58:46 +0100

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="bg-[#1f6fed]">
+    <header class="mx-auto max-w-6xl px-3 py-3">Header</header>
+  </section>
+  <main class="mx-auto max-w-6xl min-h-screen bg-red-400 py-3 px-3">
+    <slot></slot>
+  </main>
+  <footer class="mx-auto max-w-6xl bg-[#1f6fed] px-3 py-3">
+    Footer
+  </footer>
+</template>
+<script setup lang="ts">
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="bg-red-400">
+    <section class="grid grid-cols-6 gap-3">
+      <div v-for="item in 9" class="col-span-6 md:col-span-2">
+        <div class="card bg-gray-400 shadow-xl">
+          <figure>
+            <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Card Title</h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
