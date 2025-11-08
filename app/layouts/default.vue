a84c918f8a8dfeacdb446869be752f53567e4218
jonathan-martz <github@jmartz.de>
2025-11-08 15:28:01 +0100

    remove max-w

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="bg-[#1f6fed]">
-    <header class="mx-auto max-w-6xl px-3 py-3">Header</header>
+    <header class="px-3 py-3">Header</header>
   </section>
-  <main class="mx-auto max-w-6xl min-h-screen bg-red-400 py-3 px-3">
+  <main class="min-h-screen bg-red-400 py-3 px-3">
     <slot></slot>
   </main>
-  <footer class="mx-auto max-w-6xl bg-[#1f6fed] px-3 py-3">
+  <footer class="bg-[#1f6fed] px-3 py-3">
     Footer
   </footer>
 </template>
