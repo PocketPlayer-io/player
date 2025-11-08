583432fca0a1a379dce26825cf0f67c0ac6f8f57
jonathan-martz <github@jmartz.de>
2025-11-08 17:08:47 +0100

    work on mvp

---
+++ app/pages/player.vue
@@ -0,0 +1,9 @@
+<template>
+  <iframe id="player" type="text/html" width="640" height="390"
+          :src="'https://www.youtube.com/embed/'+route.query.id+'?enablejsapi=1&autoplay=0'"
+          frameborder="0"></iframe>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>#
