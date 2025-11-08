c34102a09fb3c8763aa24350f8bcf8070c6e22fe
jonathan-martz <github@jmartz.de>
2025-11-08 17:11:54 +0100

    work on mvp

--- app/pages/player.vue
+++ app/pages/player.vue
@@ -1,6 +1,6 @@
 <template>
-  <iframe id="player" type="text/html" width="640" height="390"
-          :src="'https://www.youtube.com/embed/'+route.query.id+'?enablejsapi=1&autoplay=0'"
+  <iframe id="player" type="text/html" width="100%" class="aspect-16/9"
+          :src="'https://www.youtube.com/embed/'+route.query.id+'?enablejsapi=1&autoplay=1'"
           frameborder="0"></iframe>
 </template>
 
